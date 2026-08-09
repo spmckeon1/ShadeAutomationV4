74eb6b200d82f9c2f1e8760bd53d8e425bbfe463
Stephen McKeon <stephen@rsmckeon.com>
2026-08-09 13:41:49 -0700

    Establish clean V4 application structure

--- shade_automation_v4/shadeAutomationV4.h
+++ shade_automation_v4/shadeAutomationV4.h
@@ -1,25 +1,44 @@
 #pragma once
+
 // ============================================================================
-// Shade Automation
+// Shade Automation V4
 //
 // Top-level application definition.
 //
 // The controller selection below is the single place where the target
 // controller is selected for a build.
 // ============================================================================
+
 // ============================================================================
 // CONTROLLER SELECTION
 // ============================================================================
+
 //#define SHADE_CONTROLLER_DRIVER
 #define SHADE_CONTROLLER_WINDSHIELD
 //#define SHADE_CONTROLLER_PASSENGER
 
-#if (defined(SHADE_CONTROLLER_DRIVER) + defined(SHADE_CONTROLLER_WINDSHIELD) + defined(SHADE_CONTROLLER_PASSENGER)) != 1
-  #error "Exactly one Shade Automation controller must be selected."
+#if (defined(SHADE_CONTROLLER_DRIVER) + \
+     defined(SHADE_CONTROLLER_WINDSHIELD) + \
+     defined(SHADE_CONTROLLER_PASSENGER)) != 1
+  #error "Exactly one Shade Automation V4 controller must be selected."
 #endif
 
 // ============================================================================
 // APPLICATION CONFIGURATION
 // ============================================================================
+
 #include "myConfig.h"
 
+// ============================================================================
+// APPLICATION
+// ============================================================================
+
+class ShadeAutomationV4
+{
+public:
+
+  bool startup();
+  void evtLoop();
+};
+
+extern ShadeAutomationV4 shadeAuto;
--- shade_automation_v4/shade_automation_v4.ino
+++ shade_automation_v4/shade_automation_v4.ino
@@ -1,17 +1,31 @@
-
-#include "shadeAutomation.h"
 #include "shadeAutomationV4.h"
 
 
-/*---- CALL ALL SETUP ITEMS HERE  ----*/
+ShadeAutomationV4 shadeAuto;
+
+
+/*---- PERFORM ALL NEEDED STTARTUP ACTIVITIES ----*/
+
+bool ShadeAutomationV4::startup() {
+    return true;
+}
+
+
+/*---- THE ShadeAutomationV4 CLASS EVENT LOOP  ----*/
+
+void ShadeAutomationV4::evtLoop() {
+}
+
+
+/*---- CALL ALL SETUP ITEMS HERE ----*/
 
 void setup() {
-  shadeAuto.startup();
+    shadeAuto.startup();
 }
 
-/*---- RUN THE MAIN LOOP  ----*/
+
+/*---- RUN THE MAIN LOOP ----*/
 
 void loop() {
-  shadeAuto.evtLoop();
+    shadeAuto.evtLoop();
 }
-
